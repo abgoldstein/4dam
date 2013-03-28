dece28b28bf3ec8602e797d32c68aef55b621b14
Adam Goldstein <agoldstein@mitre.org>
2013-03-28 16:24:28 -0400

    Solving up to the extra credit.

--- src/tiny-jquery.js
+++ src/tiny-jquery.js
@@ -1,1 +1,10 @@
 // Your implementation here.
+function $$(selector, initialElement) {
+  this.node = initialElement || document;
+  this.elements = this.node.querySelectorAll(selector);
+  this.elements.forEach = Array.prototype.forEach;
+
+  return this.elements;
+};
+
+var tQuery = $$;
